090611f84dd6f95a22c8ad680aaa01efaa66723d
Nikita_Lyskov <nikitam.l@mail.ru>
2023-02-15 21:30:31 +0300

    Step 1

---
+++ Program.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите a: ");
+        double a = iScanner.nextDouble();
+        System.out.printf("Введите b: ");
+        double b = iScanner.nextDouble();
+        if (pow(a, b) == 0) {
+            System.out.println("Не определено");
+        } else {
+            double c = pow(a, b);
+            System.out.print(c);
+        }
+        iScanner.close();
+    }
+
+    public static double pow(double value, double powValue) {
+        double result = 1;
+        if (powValue == 0) {
+            return result;
+        } else if (powValue < 0) {
+            powValue = powValue * -1;
+            for (int i = 1; i <= powValue; i++) {
+                result = result * value;
+            }
+            return result = 1 / result;
+        } else if (value == 0) {
+            return result = 0;
+        } else {
+            for (int i = 1; i <= powValue; i++) {
+                result = result * value;
+            }
+            return result;
+        }
+    }
+}
